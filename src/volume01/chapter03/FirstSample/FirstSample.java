c3c02bc49ff46fe3a1273ed8b77983044161220c
silentherr <silentherr@outlook.com>
2021-11-01 17:51:11 +0800

    程序清单3-1

---
+++ src/volume01/chapter03/FirstSample/FirstSample.java
@@ -0,0 +1,7 @@
+package volume01.chapter03.FirstSample;
+
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
